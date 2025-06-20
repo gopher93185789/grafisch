3b8dbc4724a7822e85ce165b564e45c7c7358a55
scott-mescudi <anish.chittu2007@gmail.com>
2025-06-20 21:12:25 +0200

    INIT

---
+++ tempura/tempuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempuraApp.swift
+//  tempura
+//
+//  Created by jayac chhitu on 19/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct tempuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
