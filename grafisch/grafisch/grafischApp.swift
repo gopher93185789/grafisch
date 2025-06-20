2f65158f3880e5b6ef8e78c3d1857be76a55b166
scott-mescudi <anish.chittu2007@gmail.com>
2025-06-21 00:49:53 +0200

    INIT

---
+++ grafisch/grafisch/grafischApp.swift
@@ -0,0 +1,17 @@
+//
+//  grafischApp.swift
+//  grafisch
+//
+//  Created by jayac chhitu on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct grafischApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
